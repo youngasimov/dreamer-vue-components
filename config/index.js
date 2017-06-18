68b7886500655d774ded39dad23bdde07f65eeca
Camilo Vera Cortés <cvera@dreamer8.com>
2017-06-18 09:32:41 +0300

    fix routes

--- config/index.js
+++ config/index.js
@@ -6,8 +6,8 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'docs',
-    assetsPublicPath: '/',
+    assetsSubDirectory: './docs',
+    assetsPublicPath: './',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -24,8 +24,8 @@ module.exports = {
   lib: {
     env: require('./prod.env'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'lib',
-    assetsPublicPath: '/',
+    assetsSubDirectory: './lib',
+    assetsPublicPath: './',
     productionSourceMap: true,
     productionGzip: false,
     productionGzipExtensions: ['js', 'css'],
